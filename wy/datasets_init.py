e83d968d68ab96c64e37e7d2da8efdef89e072fd
lucaseo <lucaseo1991@gmail.com>
2018-03-26 01:41:03 +0900

    wy_eda_update.

---
+++ wy/datasets_init.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+train = pd.read_csv("../../wy_private/walmart-trip-classification/dataset/train.csv")
+print("train data successfully imported as train")
+test = pd.read_csv("../../wy_private/walmart-trip-classification/dataset/test.csv")
+print("train data successfully imported as test")
+print("you may now begin")
