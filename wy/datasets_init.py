fdb413c7adc0d1ed36076fc41b8e78e9caf1c6d0
lucaseo <lucaseo1991@gmail.com>
2018-03-27 11:45:37 +0900

    testing slack app.

--- wy/datasets_init.py
+++ wy/datasets_init.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-train = pd.read_csv("../../wy_private/walmart-trip-classification/dataset/train.csv")
+train = pd.read_csv("../../wy_private/walmart-trip-classification/dataset/train.csv", dtype={'Upc' : object, 'FinelineNumber' : object})
 print("train data successfully imported as train")
-test = pd.read_csv("../../wy_private/walmart-trip-classification/dataset/test.csv")
+test = pd.read_csv("../../wy_private/walmart-trip-classification/dataset/test.csv", dtype={'Upc' : object, 'FinelineNumber' : object})
 print("train data successfully imported as test")
 print("you may now begin")
